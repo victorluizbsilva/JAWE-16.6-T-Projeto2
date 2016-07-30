c27232b6236da39d605d6a2e0bf7b1b5187de746
tamyrespaloma <tamyrespaloma@hotmail.com>
2016-07-30 11:31:50 -0300

    criação da classe SQL

--- Petshop/src/br/com/petshop/util/SQL.java
+++ Petshop/src/br/com/petshop/util/SQL.java
@@ -1,5 +1,11 @@
 package br.com.petshop.util;
 
 public class SQL {
-
+   public static final String INSERE = "INSERT PESSOA (CLIENTE_ID,NOME,CPF, TLEFONE, EMAIL) VALUES(?,?,?,?)";
+   public static final String REMOVE = "DELETE FROM CONTATO WHERE CLIENTE_ID = ?";
+   public static final String BUSCA_PESSOA = "SELECT * FROM PESSOA";
+   public static final String BUSCA_CPF = "SELECT * FROM PESSOA  WHERE CPF = ?";
+   public static final String BUSCA_TODOS = "SELECT * FROM PESSOA";
+   
+   
 }
